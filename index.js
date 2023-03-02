0d17f38352f23690d4cb3d5390268568f6b64dab
Haippo <kimi.haippo@opiskelija.lhkk.fi>
2023-03-02 12:21:15 +0200

    ss

---
+++ index.js
@@ -0,0 +1,76 @@
+require('dotenv').config()
+
+// Load discord.js
+const { Client, GatewayIntentBits, Events } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+
+const { DisTube } = require("distube");
+
+client.DisTube = new DisTube(client, {
+    leaveOnStop: false,
+    emitNewSongOnly: true,
+    emitAddSongWhenCreatingQueue: false,
+    emitAddListWhenCreatingQueue: false,
+})
+
+client.on("messageCreate", message => {
+    if (message.author.bot || !message.guild) return;
+    const prefix = "+"
+    const args = message.content.slice(prefix.length).trim().split(/ +/g)
+
+    if (!message.content.toLowerCase().startsWith(prefix)) return;
+
+    if (args.shift().toLowerCase() === "play") {
+        client.DisTube.play(message.member.voice.channel, args.join(" "), {
+            member: message.member,
+            textChannel: message.channel,
+            message
+        })
+    }
+})
+client.DisTube.on("playSong",(queue, song) => {
+    queue.textChannel.send("Nyt soi: " + song.name)
+})
+
+
+
+// Create a client
+//const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// This code will run once the bot has started up.
+client.on("ready", function ()
+{
+    console.log("Ready to begin! Serving in " + client.channels.length + " channels");
+});
+
+// This code will run once the bot has disconnected from Discord.
+client.on("disconnected", function () {
+    // alert the console
+    console.log("Disconnected!");
+
+    // exit node.js with an error
+    process.exit(1);
+});
+
+// This code will run once the bot receives any message.
+client.on('messageCreate', msg => {
+
+    if (msg.author.id !== client.user.id) {
+        if(msg.content.indexOf("uijuma") === 0)
+        {
+            console.log(msg.author.id);
+            //msg.channel.send(msg.content.split('').reverse().join(''));
+            msg.channel.send("tulkaa äkkiä auttaa mää oon iha jäässä");
+        }
+    }
+});
+
+client.login(process.env.TOKEN)
